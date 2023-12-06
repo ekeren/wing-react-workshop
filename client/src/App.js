7b5310446dcf6aa5d52a74c6117fc37656bb358c
Eyal Keren <eyalk@monada.co>
2023-12-06 18:48:14 +0200

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 // src/FormComponent.js
 import React, { useState } from 'react';
-​
+
 const FormComponent = () => {
   const [formData, setFormData] = useState({
     broughtToWorkshop: '',
     achievedGoals: '',
     recommendWorkshop: '',
   });
-​
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -15,17 +15,18 @@ const FormComponent = () => {
       [name]: value,
     });
   };
-​
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Form submitted:', formData);
     // You can send the form data to your backend or perform other actions
   };
-​
+
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Thank you for flying with Wing!</h2>
-​
+
       <label>
         1. What inspired you to attend the workshop?
         <input
@@ -36,7 +37,6 @@ const FormComponent = () => {
         />
       </label>
       <br />
-​
       <label>
         2. Reflecting on your goals for attending, did you achieve them? If not, what could be improved?
         <textarea
@@ -46,7 +46,6 @@ const FormComponent = () => {
         />
       </label>
       <br />
-​
       <label>
         3. Would you kindly recommend this workshop to other developers in the community? If not, we'd love to hear your thoughts on how we can improve.
         <input
@@ -57,12 +56,10 @@ const FormComponent = () => {
         />
       </label>
       <br />
-​
       <button type="submit">Submit</button>
     </form>
   );
 };
-​
 function App() {
   return (
     <div className="App">
